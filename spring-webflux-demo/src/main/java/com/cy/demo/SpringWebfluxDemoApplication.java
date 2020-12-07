370e3b71ea02b7fcf2b7b9f886dd82765d1f97fe
chenjingyue <1544552008@qq.com>
2020-12-07 22:04:25 +0800

    update pom

--- spring-webflux-demo/src/main/java/com/cy/demo/SpringWebfluxDemoApplication.java
+++ spring-webflux-demo/src/main/java/com/cy/demo/SpringWebfluxDemoApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-//@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+//@SpringBootApplication
 public class SpringWebfluxDemoApplication {
 
     public static void main(String[] args) {
